8e0addd6c8c831e174f8a2324ae3f1149e0b6255
Jake <16889000+asyncjake@users.noreply.github.com>
2025-03-11 01:15:07 -0600

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import lightwing from '@lightwing/eslint-config'
+
+export default lightwing(
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      '*.svelte',
+      '*.snap',
+      '*.d.ts',
+      'coverage',
+      'js_test',
+      'local-data',
+    ],
+  },
+  {
+    rules: {
+      'node/prefer-global/process': 'off',
+    },
+  },
+)
